fc7d673e9eff2389ee944d4e2bcd830bb425fb77
Derek Gray <tuespetre@users.noreply.github.com>
2024-11-15 14:50:36 -0600

    avoid an ArgumentException from some underbaked handling in ProjectionBubblingExpressionVisitor

--- src/Impatient/Query/ExpressionVisitors/Utility/ProjectionBubblingExpressionVisitor.cs
+++ src/Impatient/Query/ExpressionVisitors/Utility/ProjectionBubblingExpressionVisitor.cs
@@ -199,29 +199,92 @@ public class ProjectionBubblingExpressionVisitor : ExpressionVisitor
 
                 case nameof(Queryable.All):
                 case nameof(Queryable.Any):
+                case nameof(Queryable.Average):
                 case nameof(Queryable.Contains):
                 case nameof(Queryable.Count):
                 case nameof(Queryable.LongCount):
-                case nameof(Queryable.Max):
-                case nameof(Queryable.Min):
-                case nameof(Queryable.Average):
+                case nameof(Queryable.Max) when node.Type.IsScalarType():
+                case nameof(Queryable.MaxBy) when node.Type.IsScalarType():
+                case nameof(Queryable.Min) when node.Type.IsScalarType():
+                case nameof(Queryable.MinBy) when node.Type.IsScalarType():
+                case nameof(Queryable.SequenceEqual):
                 case nameof(Queryable.Sum):
                 {
                     return new ServerProjectionExpression(Expression.Default(node.Type));
                 }
+
+                case nameof(Queryable.AsQueryable):
+                case nameof(Enumerable.AsEnumerable):
+
+                case nameof(Queryable.ElementAt):
+                case nameof(Queryable.ElementAtOrDefault):
+                case nameof(Queryable.First):
+                case nameof(Queryable.FirstOrDefault):
+                case nameof(Queryable.Last):
+                case nameof(Queryable.LastOrDefault):
+                case nameof(Queryable.Single):
+                case nameof(Queryable.SingleOrDefault):
+
+                case nameof(Queryable.Distinct):
+                case nameof(Queryable.DistinctBy):
+                case nameof(Queryable.Except):
+                case nameof(Queryable.ExceptBy):
+                case nameof(Queryable.Intersect):
+                case nameof(Queryable.IntersectBy):
+                case nameof(Queryable.Union):
+                case nameof(Queryable.UnionBy):
+
+                case nameof(Queryable.Order):
+                case nameof(Queryable.OrderDescending):
+                case nameof(Queryable.OrderBy):
+                case nameof(Queryable.OrderByDescending):
+                case nameof(Queryable.ThenBy):
+                case nameof(Queryable.ThenByDescending):
+                case nameof(Queryable.Reverse):
+
+                case nameof(Queryable.Skip):
+                case nameof(Queryable.SkipLast):
+                case nameof(Queryable.SkipWhile):
+                case nameof(Queryable.Take):
+                case nameof(Queryable.TakeLast):
+                case nameof(Queryable.TakeWhile):
 
                 case nameof(Enumerable.ToArray):
                 case nameof(Enumerable.ToDictionary):
                 case nameof(Enumerable.ToHashSet):
                 case nameof(Enumerable.ToList):
                 case nameof(Enumerable.ToLookup):
-                default:
                 {
                     if (arguments[0] is ProjectionExpression projection)
                     {
                         return projection;
                     }
 
+                    return node;
+                }
+
+                // for the following operators, as well as any new operators that might come along,
+                // we either cannot reliably bubble a projection out of them, or we need to give them
+                // more thoughtful consideration.
+
+                case nameof(Queryable.Aggregate):
+
+                case nameof(Queryable.Append):
+                case nameof(Queryable.Concat):
+                case nameof(Queryable.Prepend):
+
+                case nameof(Queryable.Cast):
+                case nameof(Queryable.OfType):
+
+                case nameof(Queryable.DefaultIfEmpty):
+
+                case nameof(Queryable.Chunk):
+                case nameof(Enumerable.Empty):
+                case nameof(Enumerable.Range):
+                case nameof(Enumerable.Repeat):
+
+                default:
+                {
                     return node;
                 }
             }
